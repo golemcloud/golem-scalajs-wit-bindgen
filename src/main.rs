69bcab43ea949f482b70d087ddce9cff6a50918a
Jorge Vasquez <jorge.vasquez0123@proton.me>
2023-09-25 10:39:52 -0400

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
-use std::path::Path;
+use std::{fs, path::Path};
 
 use cargo_scalajs_wit_bindgen::codegen::Interface;
+use clap::Parser;
 use wit_parser::SourceMap;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    /// The path to the input WIT file
+    #[arg(short, long)]
+    wit: String,
+
+    /// The package of the generated Api.scala file
+    #[arg(short, long)]
+    package: String,
+}
+
 fn main() {
+    let cli_args = CliArgs::parse();
+
     let mut source = SourceMap::new();
-    source.push_file(Path::new("main.wit")).unwrap();
+    source.push_file(Path::new(&cli_args.wit)).unwrap();
     let unresolved_package = source.parse().unwrap();
 
-    // println!(
-    //     "{:#?}",
-    //     unresolved_package
-    //         .types
-    //         .iter()
-    //         //.map(|(_, ty)| ty)
-    //         .collect::<Vec<_>>()
-    // );
+    let dest_dir = format!("src/main/scala/{}", cli_args.package.replace(".", "/"));
 
-    // println!(
-    //     "{:#?}",
-    //     unresolved_package
-    //         .interfaces
-    //         .iter()
-    //         .find(|(id, _)| *id == interface_id)
-    //         .unwrap()
-    // );
+    fs::create_dir_all(&dest_dir).unwrap();
 
-    println!(
-        "{}",
-        Interface::from_wit(&unresolved_package).render("example")
-    );
+    fs::write(
+        format!("{dest_dir}/Api.scala"),
+        Interface::from_wit(&unresolved_package).render(&cli_args.package),
+    )
+    .unwrap()
 }
